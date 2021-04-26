27e871764795c4fe20b0dd21a7506bb28a002c78
kjbennicke <56367861+kjbennicke@users.noreply.github.com>
2021-04-25 18:10:34 -0700

    Flattened game functionality and added in a check win and loss conditions.

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -12,7 +12,7 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     //Game loop win or lose.
-    if(bGameOver || Lives < 1){
+    if(bGameOver){
         ClearScreen();
         SetupGame();
     }
@@ -34,29 +34,39 @@ void UBullCowCartridge::SetupGame(){
 }
 void UBullCowCartridge::EndGame(){
     bGameOver = true;
+    PrintLine(TEXT("Press enter to play again..."));
 }
-void UBullCowCartridge::ProcessGuess(FString Input){
-    ClearScreen();
-    if(Input == HiddenWord){
+void UBullCowCartridge::ProcessGuess(FString Guess){
+    if(Guess == HiddenWord){
+        ClearScreen();
         PrintLine(TEXT("You Guessed it!"));
         EndGame();
+        return;
     }
-    else{
-        if(Input.Len() != HiddenWord.Len()){
-            PrintLine(TEXT("That is not the right amount of characters..."));
-            EndGame();
-            return;
-        }
-        PrintLine(TEXT("...That is not correct."));
-        PrintLine(TEXT("You have %i Lives left."), --Lives);
+    if(Guess.Len() != HiddenWord.Len()){
+        ClearScreen();
+        PrintLine(TEXT("That is not the right amount of characters..."));
+        PrintLine(TEXT("You have a %i lives left."), Lives);
+        return;
+    }
+    //everything below removed a life.
+    --Lives;
+    if(Lives > 0){
+        ClearScreen();
+        PrintLine(TEXT("That is incorrect.\nYou have %i lives left."), Lives);
+        return;
+    }
+    if(Lives <= 0){
+        ClearScreen();
+        PrintLine(TEXT("You are out of lives!"));    
+        PrintLine(TEXT("The hidden word was '%s'."), *HiddenWord);    
+        EndGame();
+        return;
     }
     //Check if isogram
     //Prompt to guess again
     //check is number of characters is correct 
     //prompt to guess again
-
-    //Remove life
-
     //Check if lives > 0
     //if lives greater than 0, go back to  loop and try again.
     //else if lives are 0 or less, Game Over prompt to play again
